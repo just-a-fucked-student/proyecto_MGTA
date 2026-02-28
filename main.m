98f26cde53b2db204bef0329be37d94167d5c311
Eric <eric.comaposada@estudiantat.upc.edu>
2026-02-28 11:32:03 +0000

    Grafico demanda - capacidad

--- main.m
+++ main.m
@@ -9,10 +9,29 @@ aeropuerto = "LEBL_10AUG2025.xlsx";
 data = readtable(aeropuerto);
 %Guarda solo la segunda hoja del excel(llegadas)
 llegadas = readtable(aeropuerto,'Sheet','LLEGADAS');
-
+horas_vuelos = llegadas.ETA *24;  
 % disp("Primeras 8 filas:")   //Test
 % disp(head(data,8))
 
+AAR = 40;
+PAAR = 20;
+Hfile = 6;
+Hstart = 8;
+Hend = 14;
 
+%Gráfico demanda-capacidad
+figure;
+histogram(horas_vuelos, 0:24, 'FaceColor', [0.2 0.2 0.6]); 
+hold on; % Congelem el gràfic per dibuixar a sobre
 
+plot([0, Hstart], [AAR, AAR], 'g', 'LineWidth', 2);
+plot([Hstart, Hend], [PAAR, PAAR], 'r', 'LineWidth', 2);
+plot([Hend, 24], [AAR, AAR], 'g', 'LineWidth', 2);
 
+title('Histogram of arrivals on non-regulated traffic');
+xlabel('Time in UTC (hours)');
+ylabel('Arrivals (number of aircraft)');
+xlim([0, 24]);          
+ylim([0, AAR + 10]);
+
+hold off;
