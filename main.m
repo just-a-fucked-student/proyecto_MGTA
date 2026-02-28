f368a7713ce647c1cd62b56ee1f5c41b6c7eac0c
Eric <eric.comaposada@estudiantat.upc.edu>
2026-02-28 10:44:14 +0000

    Datos excel puestos

--- main.m
+++ main.m
@@ -2,10 +2,13 @@ clearvars
 clc
 close all
 
-%Cargar Datos desde el csv
+%Cargar Datos desde el xlsx
 
-aeropuerto = "LEBL.csv"
+aeropuerto = "LEBL_10AUG2025.xlsx";
+%Guarda todos los datos (lee la primera hoja)
 data = readtable(aeropuerto);
+%Guarda solo la segunda hoja del excel(llegadas)
+llegadas = readtable(aeropuerto,'Sheet','LLEGADAS');
 
 % disp("Primeras 8 filas:")   //Test
 % disp(head(data,8))
